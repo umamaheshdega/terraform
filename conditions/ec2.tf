615d2d13317172fb4569e527f8ed1eae61f9f075
umamaheshdega@umadega <mahesh445667@gmail.com>
2025-06-24 10:12:09 +0530

    terraform_pratice;

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "" {
+resource "aws_instance" "mahesh" {
   ami                    = var.ami_id = "prod"? "ami-09c813fb71547fc4f": "ami-07041441b708acbd6"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   instance_type          = var.environment = "prod"? "t2.small": "t2.micro"
